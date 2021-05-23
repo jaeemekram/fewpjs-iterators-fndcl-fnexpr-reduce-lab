9e1d5879017bbcb2befddb3d8dd4d8b65da524f9
Jaeem Ekram <jekram98@gmail.com>
2021-05-23 18:19:16 -0400

    commit tree c80c19b16d3ae8e270148cfd19bdee7f7165f133

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce((assembledBatteries, batteryBatches) => assembledBatteries + batteryBatches, 0 )
+
+const wordCountMap = monologueLines.reduce((wordPerSentence, freq) =>  {
+  const wordCount = freq.split(' ').length;
+
+  if (!wordPerSentence[wordCount]) {
+    wordPerSentence[wordCount] = 0;
+  }
+
+ wordPerSentence[wordCount]++;
+
+  return wordPerSentence
+}, {})
